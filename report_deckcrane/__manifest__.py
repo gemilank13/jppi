2354fc1b9be40d84b9c13428b8cbec22ba767744
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2022-04-25 10:00:11 +0700

    Add files via upload

---
+++ report_deckcrane/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "JPPI - Report Deckcrane",
+
+    'summary': """ Inherit untuk print out pdf """,
+    'description': """Inherit untuk print out pdf """,
+    'author': "PT JPPI - Geger Gemilank",
+    'website': "http://www.jppi.co.id",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base','deckcrane'],
+
+    'data': [
+         'reports/report_pdf.xml',
+         'reports/inspection_report.xml',
+
+    ],
+    
+    'demo': [],
+}
