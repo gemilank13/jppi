f7bbb68fea126641a7da4e3748ad693d6b8cd6dc
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2022-05-30 14:44:40 +0700

    Delete perkara_hukum directory

--- perkara_hukum/models/klasifikasi_perkara.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class KlasifikasiPerkara(models.Model):
-    _name = 'klasifikasi.perkara'
-    
-    name = fields.Char('Klasifikasi Perkara')
--- perkara_hukum/models/lampiran.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Lampiran(models.Model):
-    _name = 'lampiran.lampiran'
-    
-    name = fields.Binary(string="Lampiran")
-    perkara_hukum_id = fields.Many2one('perkara.hukum', readonly=True)
